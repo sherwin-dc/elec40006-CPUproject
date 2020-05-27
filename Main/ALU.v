51ec25bd871f4e57fa9840da44685daab36e34cd
sherwin-dc <swd18@ic.ac.uk>
2020-05-27 12:20:49 +0800

    done with ALU

--- Main/ALU.v
+++ Main/ALU.v
@@ -1,14 +1,13 @@
 module ALU
 (
-	input [15:0] rddata, rsdata, rmdata, N, instruction,
-	
+	input [15:0] rsdata, rmdata, N,
+	input instr_bit_15, 
+	input [1:0] instr_bit_12_11,
 	output [15:0] aluout
 );
 
 
-wire [3:0] opcode = instruction[15:12];
-wire type = instruction[11]; //type R = 0, type I = 1
-wire [2:0] Rd = instruction[10:8];
+wire [2:0] opcode = { instr_bit_15 , instr_bit_12_11[1:0] };
 
 reg [15:0] alusum;
 
@@ -17,18 +16,13 @@ assign aluout = alusum;
 always @(*)
 	begin 
 		case (opcode)
-			5'b01000 : alusum = rsdata + rmdata; // ADD R
-			5'b01001 : alusum = rsdata + N; // ADD I
-			5'b01010 : alusum = rsdata - rmdata; // SUB R
-			5'b01011 : alusum = rsdata - N; // SUB I
-			5'b11000 : alusum = {rsdata[14:0], 1'b0}; // LSL
-			5'b11010 : alusum = {1'b0, rsdata[15:1]}; // LSR (possibly ASR implementation as well)
-			5'b01100 : alusum = rddata * rsdata + rmdata; //MAS R
-//			5'b01101 : alusum = 0; // MAS I
-//			5'b01110 : alusum = rsdata; // MOV R
-//			5'b01111 : alusum = N; // MOV I
-			
-			
+			3'b000 : alusum = rsdata + rmdata; // ADD R
+			3'b001 : alusum = rsdata + N; // ADD I
+			3'b010 : alusum = rsdata - rmdata; // SUB R
+			3'b011 : alusum = rsdata - N; // SUB I
+			3'b100 : alusum = {rsdata[14:0], 1'b0}; // LSL
+			3'b110 : alusum = {1'b0, rsdata[15:1]}; // LSR (possibly ASR implementation as well)
+					
 			default : alusum = 0;
 		endcase;
 	end
